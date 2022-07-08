b180d198a3608874653245a8f935e9538fbd09fe
Arnaiz23 <aaf110501@gmail.com>
2022-07-08 19:27:34 +0200

    dockerState

---
+++ README.md
@@ -0,0 +1,3 @@
+# This repository is for all the scripts that I create and I use
+
+- dockerState.sh -> Change the environment in docker-compose
---
+++ dockerState.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PS3="Seleccionar una opcion: "
+
+select option in Prod Dev Down Exit
+do
+	case $option in
+		Prod)
+			echo "Arrancando docker en Produccion"
+			docker compose up
+			echo "Exito!!!"
+			break
+			;;
+		Dev)
+			echo "Arrancando docker en Development"
+			docker compose -f docker-compose-dev.yml up
+			echo "Exito!!!"
+			break
+			;;
+		Down)
+			echo "Eliminando los contenedores"
+			docker compose down
+			echo "|-------------------------------------------------------------------------|"
+			echo "|  ¡¡¡¡¡¡Recuerde eliminar la imagen si quieres cambiar de entorno!!!!!!  |"
+			echo "|--------------------------------------------------------------------------|"
+			break
+			;;
+		Exit)
+			echo "Saliendo..."
+			break
+			;;
+	esac
+done
