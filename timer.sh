98a2c2781b7c8d89f8da3f8324b9eb1481c56ba1
Arnaiz23 <aaf110501@gmail.com>
2023-02-02 13:28:40 +0100

    feat: 🎸 Implement music to the final countdown

--- timer.sh
+++ timer.sh
@@ -12,3 +12,5 @@ echo "Countdown: $minutes minutes"
 while [ "$date1" -ge `date +%s` ]; do 
 	echo -ne "$(date -u --date @$(($date1 - `date +%s` )) +%H:%M:%S)\r"; 
 done
+
+cvlc ~/Música/Rihanna_Love_The_Way_You_Lie_Part_II__\(thinkNews.com.ng\).mp3
